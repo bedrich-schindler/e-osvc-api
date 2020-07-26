8765cd417549bb262adf8730e2d1a8bae97733af
Bedrich Schindler <bedrich.schindler@gmail.com>
2020-07-26 21:18:17 +0200

    Move UserPasswordSubscriber logic into UserSubscriber

--- src/Subscriber/UserSubscriber.php
+++ src/Subscriber/UserSubscriber.php
@@ -10,6 +10,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserSubscriber implements EventSubscriberInterface
 {
@@ -18,17 +19,26 @@ class UserSubscriber implements EventSubscriberInterface
      */
     private EntityManagerInterface $entityManager;
 
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private UserPasswordEncoderInterface $passwordEncoder;
 
-    public function __construct(EntityManagerInterface $entityManager) {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        UserPasswordEncoderInterface $passwordEncoder
+    ) {
         $this->entityManager = $entityManager;
+        $this->passwordEncoder = $passwordEncoder;
     }
-
 
     public static function getSubscribedEvents(): array
     {
         return [
             KernelEvents::VIEW => [
-                ['addUser', EventPriorities::POST_WRITE],
+                ['addUserPre', EventPriorities::PRE_VALIDATE],
+                ['addUserPost', EventPriorities::POST_WRITE],
+                ['editUserPre', EventPriorities::PRE_VALIDATE],
             ],
         ];
     }
@@ -36,7 +46,27 @@ class UserSubscriber implements EventSubscriberInterface
     /**
      * @param ViewEvent $event
      */
-    public function addUser(ViewEvent $event): void
+    public function addUserPre(ViewEvent $event): void
+    {
+        $entity = $event->getControllerResult();
+
+        if (!$entity instanceof User) {
+            return;
+        }
+
+        $method = $event->getRequest()->getMethod();
+
+        if (Request::METHOD_POST !== $method) {
+            return;
+        }
+
+        $this->encodePassword($entity);
+    }
+
+    /**
+     * @param ViewEvent $event
+     */
+    public function addUserPost(ViewEvent $event): void
     {
         $entity = $event->getControllerResult();
 
@@ -55,5 +85,39 @@ class UserSubscriber implements EventSubscriberInterface
 
         $this->entityManager->persist($userNotifications);
         $this->entityManager->flush();
+    }
+
+    /**
+     * @param ViewEvent $event
+     */
+    public function editUserPre(ViewEvent $event): void
+    {
+        $entity = $event->getControllerResult();
+
+        if (!$entity instanceof User) {
+            return;
+        }
+
+        $method = $event->getRequest()->getMethod();
+
+        if (Request::METHOD_PUT !== $method) {
+            return;
+        }
+
+        $this->encodePassword($entity);
+    }
+
+
+    private function encodePassword(User $entity): void
+    {
+        if (null === $entity->getPlainPassword()) {
+            return;
+        }
+
+        $encoded = $this->passwordEncoder->encodePassword(
+            $entity,
+            $entity->getPlainPassword()
+        );
+        $entity->setPassword($encoded);
     }
 }
